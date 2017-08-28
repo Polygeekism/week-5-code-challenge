0c7844da6c5825b3e276858efddda98d313c0008
polygeekism <mcmahon.kevin.t@gmail.com>
2017-08-28 15:23:07 -0500

    ng-models setup for input of new messages

--- public/client.js
+++ public/client.js
@@ -4,4 +4,6 @@ var messageApp = angular.module('MessageApp', []);
 
 messageApp.controller('MessageController', ['$http', function($http){
     console.log('Message controller loaded');
+    var self = this;
+    self.newMessage = {};
 }])
