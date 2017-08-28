5691ed5ea187e0c7f509c67ecaa6b0308d8a68c3
polygeekism <mcmahon.kevin.t@gmail.com>
2017-08-28 15:33:11 -0500

    connection to mongo db created. Server router created and linked with server

--- public/client.js
+++ public/client.js
@@ -6,4 +6,16 @@ messageApp.controller('MessageController', ['$http', function($http){
     console.log('Message controller loaded');
     var self = this;
     self.newMessage = {};
+
+    //post route
+    self.postMessage = function(messageToAdd){
+        console.log('ng click route is linked:', messageToAdd);
+        self.newMessage={};//clear input boxes
+    }
+    self.getMessages = function(){
+        
+        $http.get('/messages', function(response){
+            console.log()
+        })
+    }
 }])
--- server/app.js
+++ server/app.js
@@ -2,10 +2,26 @@ var express = require('express')
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var messages = require('./routes/messages.js')
 
 
 //middleware
 app.use(express.static('public'));
+
+app.use('/messages', messages);
+
+
+var databaseUrl = 'mongodb://localhost:27017/mongodbPractice';
+mongoose.connect(databaseUrl,
+    {
+        useMongoClient: true
+    });
+mongoose.connection.on('connected', function () {
+    console.log('mongoose connected to : ', databaseUrl);
+});
+mongoose.connection.on('error', function (err) {
+    console.log('mongoose connection error to : ', err);
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
